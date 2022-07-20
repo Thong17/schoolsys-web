606f4c7934a2e21761c32434e36304151bfb3ccf
SATHAPANA\chhan.bunthong <chhan.bunthong@sathapana.com.kh>
2022-07-20 23:53:17 +0700

    [update] - add server pagination attendance

--- src/modules/operation/attendance/index.tsx
+++ src/modules/operation/attendance/index.tsx
@@ -15,9 +15,23 @@ import useTheme from 'hooks/useTheme'
 import Breadcrumb from 'components/shared/Breadcrumbs'
 import { SearchField } from 'components/shared/table/SearchField'
 import { FilterButton } from 'components/shared/table/FilterButton'
-import { MenuList } from '@mui/material'
+import { MenuItem } from '@mui/material'
+import { SortIcon } from 'components/shared/icons/SortIcon'
 
-const Header = ({ onSearch }) => {
+const Header = ({ onSearch, handleFilter }) => {
+  const [sortObj, setSortObj] = useState({
+    name: false,
+    createdAt: false,
+    room: false,
+    schedule: false,
+    isActive: false,
+    startedAt: false,
+  })
+
+  const handleChangeFilter = ({ filter }) => {
+    setSortObj({ ...sortObj, [filter]: !sortObj[filter] })
+    return handleFilter({ filter, asc: sortObj[filter] })
+  }
   const stages = [
     {
       title: 'Operation',
@@ -33,8 +47,12 @@ const Header = ({ onSearch }) => {
     <div style={{ display: 'flex', alignItems: 'center' }}>
       <SearchField onChange={(e) => onSearch(e)} />
       <FilterButton style={{ marginLeft: 10 }}>
-        <MenuList>Sort By Name</MenuList>
-        <MenuList>Sort By Date</MenuList>
+        <MenuItem onClick={() => handleChangeFilter({ filter: 'name' })}><SortIcon asc={sortObj.name} /> By Class</MenuItem>
+        <MenuItem onClick={() => handleChangeFilter({ filter: 'createdAt' })}><SortIcon asc={sortObj.createdAt} /> By Date Created</MenuItem>
+        <MenuItem onClick={() => handleChangeFilter({ filter: 'room' })}><SortIcon asc={sortObj.room} /> By Room</MenuItem>
+        <MenuItem onClick={() => handleChangeFilter({ filter: 'schedule' })}><SortIcon asc={sortObj.schedule} /> By Schedule</MenuItem>
+        <MenuItem onClick={() => handleChangeFilter({ filter: 'isActive' })}><SortIcon asc={sortObj.isActive} /> By Active</MenuItem>
+        <MenuItem onClick={() => handleChangeFilter({ filter: 'startedAt' })}><SortIcon asc={sortObj.startedAt} /> By Date Started</MenuItem>
       </FilterButton>
     </div>
   </>
@@ -47,17 +65,39 @@ export const Classes = () => {
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
   const [rowData, setRowData] = useState<Data[]>([])
-  const { data: classes, status } = useAppSelector(selectListClass)
+  const { data: classes, count, status } = useAppSelector(selectListClass)
   const [queryParams, setQueryParams] = useSearchParams()
-  const [loading, setLoading] = useState(status === 'LOADING' ? true : false)
 
   const updateQuery = debounce((value) => {
-    setLoading(false)
-    setQueryParams({ search: value })
+    handleQuery({ search: value })
   }, 300)
 
   const handleSearch = (e) => {
     updateQuery(e.target.value)
+  }
+
+  const handleFilter = (option) => {
+    handleQuery({ filter: option.filter, sort: option.asc ? 'asc' : 'desc' })
+  }
+
+  const handleQuery = (data) => {
+    let { limit, search } = data
+
+    let query = {}
+    const _limit = queryParams.get('limit')
+    const _page = queryParams.get('page')
+    const _search = queryParams.get('search')
+    const _filter = queryParams.get('filter')
+    const _sort = queryParams.get('sort')
+
+    if (_limit) query = { limit: _limit, ...query }
+    if (_page) query = { page: _page, ...query }
+    if (_search) query = { search: _search, ...query }
+    if (_filter) query = { filter: _filter, ...query }
+    if (_sort) query = { sort: _sort, ...query }
+
+    if (limit || search) return setQueryParams({...query, ...data, page: 0})
+    setQueryParams({...query, ...data})
   }
   
   useEffect(() => {
@@ -91,9 +131,16 @@ export const Classes = () => {
 
   return (
     <Container
-      header={<Header onSearch={handleSearch} />}
+      header={<Header onSearch={handleSearch} handleFilter={handleFilter} />}
     >
-      <StickyTable columns={columnData} rows={rowData} loading={loading} />
+      <StickyTable 
+        columns={columnData} 
+        rows={rowData}        
+        setQuery={handleQuery}
+        count={count}
+        limit={parseInt(queryParams.get('limit') || '10')}
+        skip={status === 'SUCCESS' ? parseInt(queryParams.get('page') || '0') : 0} 
+      />
     </Container>
   )
 }
