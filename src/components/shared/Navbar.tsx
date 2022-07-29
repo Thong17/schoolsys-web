dd9228cfb9569f73ef828ebcb43d5e1554b86240
SATHAPANA\chhan.bunthong <chhan.bunthong@sathapana.com.kh>
2022-07-29 19:19:28 +0700

    [update] - school logo

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,4 +1,4 @@
-import logo from '../../assets/logo.jpg'
+import logo from '../../assets/logo.png'
 import useAuth from 'hooks/useAuth'
 import useTheme from 'hooks/useTheme'
 import { Link } from 'react-router-dom'
