cd5b3ab69ece2f7e4efef9c81968111beea2c157
SATHAPANA\chhan.bunthong <chhan.bunthong@sathapana.com.kh>
2022-11-16 21:41:19 +0700

    [update] - expand sidebar

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -4,11 +4,29 @@ import useTheme from 'hooks/useTheme'
 import { CustomSideNav, SideNavContainer } from 'styles'
 import useConfig from 'hooks/useConfig'
 import useLanguage from 'hooks/useLanguage'
+import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded'
+import ExpandLessRoundedIcon from '@mui/icons-material/ExpandLessRounded'
+import { useState } from 'react'
 
 const Sidebar = () => {
   const { theme } = useTheme()
   const { language } = useLanguage()
-  const { sidebar } = useConfig()
+  const { sidebar, tabs, resetTabs } = useConfig()
+  const [expandTabs, setExpandTabs] = useState<string[]>(tabs)
+
+  const handleExpandLessTabs = (event, tab) => {
+    const newTabs = expandTabs.filter(item => item !== tab)
+    setExpandTabs(newTabs)
+    resetTabs(newTabs)
+    event.preventDefault()
+  }
+
+  const handleExpandMoreTabs = (event, tab) => {
+    const newTabs = [...expandTabs, tab]
+    setExpandTabs(newTabs)
+    resetTabs(newTabs)
+    event.preventDefault()
+  }
 
   return (
     <SideNavContainer open={sidebar}>
@@ -19,12 +37,34 @@ const Sidebar = () => {
         className='side-nav'
         styled={theme}
       >
-        {sideNav.map((nav, index) => (
-          <NavLink key={index} to={nav.route}>
-            {nav.icon}
-            <span>{language?.[nav.title] || nav.title}</span>
-          </NavLink>
-        ))}
+        {sideNav.map((nav, index) => {
+          if (!nav.children) {
+            return <NavLink key={index} to={nav.route}>
+              {nav.icon}
+              <span>{language?.[nav.title] || nav.title}</span>
+            </NavLink>
+          }
+          
+          return <div key={index} className='link'>
+            <NavLink key={index} to={nav.route} style={{ width: 'calc(100% - 22px)', paddingRight: 7, display: 'flex', alignItems: 'center', justifyContent: 'space-between', boxSizing: 'border-box' }}>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                {nav.icon}
+                <span>{language?.[nav.title] || nav.title}</span>
+              </div>
+              {expandTabs.includes(nav.title) ? <ExpandLessRoundedIcon className='toggle' onClick={(event) => handleExpandLessTabs(event, nav.title)} /> : <ExpandMoreRoundedIcon className='toggle' onClick={(event) => handleExpandMoreTabs(event, nav.title)} />}
+            </NavLink>
+            {expandTabs.includes(nav.title) && <div style={{ paddingLeft: 10, boxSizing: 'border-box' }}>
+              {
+                nav.children.map((sub, subIndex) => (
+                  <NavLink key={subIndex} to={sub.route}>
+                    {sub.icon}
+                    <span>{language?.[sub.title] || sub.title}</span>
+                  </NavLink>
+                ))
+              }
+            </div>}
+          </div>
+        })}
       </CustomSideNav>
     </SideNavContainer>
   )
