aaed38dddc174ee4f1c47a9f2eee9908820ebafe
SATHAPANA\chhan.bunthong <chhan.bunthong@sathapana.com.kh>
2022-11-16 21:51:20 +0700

    [update] - remove navbar on school

--- src/modules/school/components/Navbar.tsx
+++ src/modules/school/components/Navbar.tsx
@@ -1,16 +1,16 @@
-import useLanguage from 'hooks/useLanguage'
+// import useLanguage from 'hooks/useLanguage'
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+// import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
-  const { language } = useLanguage()
+  // const { language } = useLanguage()
   return (
     <>
-      <NavLink to='/school/attendance'>{language['ATTENDANCE']}</NavLink>
+      {/* <NavLink to='/school/attendance'>{language['ATTENDANCE']}</NavLink>
       <NavLink to='/school/teacher'>{language['TEACHER']}</NavLink>
       <NavLink to='/school/grade'>{language['GRADE']}</NavLink>
       <NavLink to='/school/class'>{language['CLASS']}</NavLink>
-      <NavLink to='/school/student'>{language['STUDENT']}</NavLink>
+      <NavLink to='/school/student'>{language['STUDENT']}</NavLink> */}
     </>
   )
 }
